3320177965829db6d68f1c7377f4443c55daa6ab
MR-HENDRA <hunterblackshadow26@gmail.com>
2024-01-13 00:56:40 +0800

    Update ji lagi

--- Belajar Git/BelajarGit.js
+++ Belajar Git/BelajarGit.js
@@ -3,3 +3,5 @@ const add = (a, b) => a + b;
 console.log(add(1, 2));
 
 // 1. git diff -> Melihat Perbedaan atau update pada code file.
+// 2. git log -> Melihat History Commit
+// 2. git log --oneline -> Melihat History Commit secara singkat
