0a276654fc96640404a6930f1a0f7d85e772e2b4
MR-HENDRA <hunterblackshadow26@gmail.com>
2024-01-13 10:47:41 +0800

    Update

--- Belajar Git/BelajarGit.js
+++ Belajar Git/BelajarGit.js
@@ -7,3 +7,4 @@ console.log('Hello');
 // 3. git log --oneline -> Melihat History Commit secara singkat
 // 4. git restore -> Membatalkan perubahan code / mengembalikan code ke perubahan terakhir pas commit.
 // 5. git restore -S -> Membatalakan perubahan pada file/code yang telah di git add (sudah masuk ke Sketching area)
+// 6. git reset --hard kodehas -> Mengembalikkan ke Commit tertentu
