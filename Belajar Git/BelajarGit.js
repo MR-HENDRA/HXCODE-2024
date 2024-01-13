92a0108322526f1742643cd08c66d1bb7bbd1d19
MR-HENDRA <hunterblackshadow26@gmail.com>
2024-01-13 10:33:00 +0800

    Belajar Git

--- Belajar Git/BelajarGit.js
+++ Belajar Git/BelajarGit.js
@@ -1,8 +1,9 @@
 console.log('Belajar Git');
 const add = (a, b) => a + b;
 console.log(add(1, 2));
-
+console.log('Hello');
 // 1. git diff -> Melihat Perbedaan atau update pada code file.
 // 2. git log -> Melihat History Commit
 // 3. git log --oneline -> Melihat History Commit secara singkat
 // 4. git restore -> Membatalkan perubahan code / mengembalikan code ke perubahan terakhir pas commit.
+// 5. git restore -S -> Membatalakan perubahan pada file/code yang telah di git add (sudah masuk ke Sketching area)
