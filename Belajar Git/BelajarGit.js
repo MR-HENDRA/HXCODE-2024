078cc44751a9b45ff5c60226012f8512be00aaa1
MR-HENDRA <hunterblackshadow26@gmail.com>
2024-01-13 10:17:12 +0800

    Update

--- Belajar Git/BelajarGit.js
+++ Belajar Git/BelajarGit.js
@@ -4,4 +4,5 @@ console.log(add(1, 2));
 
 // 1. git diff -> Melihat Perbedaan atau update pada code file.
 // 2. git log -> Melihat History Commit
-// 2. git log --oneline -> Melihat History Commit secara singkat
+// 3. git log --oneline -> Melihat History Commit secara singkat
+// 4. git restore -> Membatalkan perubahan code / mengembalikan code ke perubahan terakhir pas commit.
